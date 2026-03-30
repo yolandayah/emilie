107eb1329679fd8afd4984649d46ddf05d0030aa
Yolanda Alejo Huerta <za240113432@zapopan.tecmm.edu.mx>
2026-03-29 18:18:34 -0600

    components: update layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-</div>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="utf-8" />
+ <meta http-equiv="x-ua-compatible" content="ie=edge" />
+ <meta name="viewport" content="width=device-width, initial-scale=1" />
+ <!--
+ <link rel="stylesheet" href="css/base.css">
+ <link rel="icon" href="img/favicon.png">
+ -->
+ <title>Login & Registration</title>
+ </head>
+<body>
+  <main>
+    {{ $slot }}
+  </main>
+</body>
+</html>
+<!-- vi: set filetype=php: -->
