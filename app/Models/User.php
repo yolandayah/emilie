d5baa8ab6ac5246f0c0fb47fd6b55247fd47dbe3
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2026-04-08 23:16:47 -0600

    install Laravel Permission
    From [1], install Laravel Permission
    
    [1] https://spatie.be/docs/laravel-permission/v7/introduction

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['username', 'email', 'name', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -16,6 +17,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * Get the attributes that should be cast.
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use Spatie\Permission\PermissionServiceProvider;
 
 return [
     AppServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
 ];
