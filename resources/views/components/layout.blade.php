bef00c4fa8b926684945e04218aad3180f15f38d
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2026-04-10 04:17:47 -0600

    usando custom.css en vez de base.css
    No se porque no funciona con base.css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,6 @@
  <meta name="viewport" content="width=device-width, initial-scale=1" />
  <!--
  <link rel="icon" href="img/favicon.png">
- <link rel="stylesheet" href="/css/base.css">
  -->
  <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
  <link rel="stylesheet" href="/css/custom.css">
