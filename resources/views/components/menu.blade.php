6c414bb99ebb2399be0343234614ef39e76d0075
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2026-04-10 03:59:34 -0600

    agregar el componente de menu

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,6 @@
+<nav>
+<a class="current" href="{{ route('home') }}">Home</a>
+<a href="{{ route('register') }}">Registrate</a>
+<a href="{{ route('login') }}">Inicia sesión</a>
+</nav>
+<!-- vi: set filetype=php: -->
