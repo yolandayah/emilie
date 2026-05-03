0d31871530e8e4d4cbb108629799b9b55f537ecf
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2026-05-03 10:01:12 -0600

    script: add minify script and mini versions for local files

---
+++ scripts/minify-css-js.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+# vi: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Dependencias:
+# composer require  matthiasmullie/minify --dev
+
+DIRECTORY=${1:-.}
+MCSS=vendor/bin/minifycss
+MJS=vendor/bin/minifyjs
+
+if [ ! -d ${DIRECTORY}/public/css ]
+then
+    echo No se encuenta el directorio ${DIRECTORY}/public/css
+    exit 1
+fi
+
+if [ ! -x ${MCSS} ]
+then
+    echo No se encuenta el archivo ${MCSS}
+    exit 1
+fi
+
+if [ ! -x ${MJS} ]
+then
+    echo No se encuenta el archivo ${MJS}
+    exit 1
+fi
+
+CSS=$(ls ${DIRECTORY}/public/css)
+JS=$(ls ${DIRECTORY}/public/js/*.js)
+
+for f in $CSS
+do
+    case $f in
+        *"min"*)
+            continue
+            ;;
+        *)
+            filename=${f%*.css}
+            $MCSS ${DIRECTORY}/public/css/$f > ${DIRECTORY}/public/css/${filename}.min.css
+            ;;
+    esac
+done
+
+for f in $JS
+do
+    case $f in
+        *"min"*)
+            continue
+            ;;
+        *)
+            filename=${f%*.js}
+            $MJS $f > ${filename}.min.js
+            ;;
+    esac
+done
