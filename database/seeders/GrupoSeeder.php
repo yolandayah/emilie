b4fd22b4985533e1cfb50529c16d9e491bd56a5d
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2026-05-09 11:33:45 +0000

    Seeder: add another user to group

--- database/seeders/GrupoSeeder.php
+++ database/seeders/GrupoSeeder.php
@@ -11,11 +11,17 @@ class GrupoSeeder extends Seeder
     public function run(): void
     {
         for ($i = 1; $i <= 24; $i++) {
+            $uid = 1;
+            if ($i>12) {
+                $uid = ($i+1) % 3 + 2;
+            } else {
+                $uid = $i % 3 + 2;
+            }
             $g = Grupo::create([
                 'nombre' => "Grupo $i",
                 'archivar' => false,
                 'asignatura_id' => $i % 12 + 1,
-                'user_id' => $i % 3 + 2,
+                'user_id' => $uid,
             ]);
             $userIds = [];
             for ($u = 0; $u < 10; $u++) {
